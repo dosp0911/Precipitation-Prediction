a839ad82bf49179891da5c5179b2dd1c05be8ca9
dosp0911 <dosp0911@gmail.com>
2020-05-03 02:21:33 +0900

    add preprocss

---
+++ pre_processing.py
@@ -0,0 +1,47 @@
+import cv2
+import numpy as np
+import util
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+
+def handle_outliers(arr):
+    """
+    convert outlier values(-9999, nan) into 0
+    :param arr: numpy arr
+    :return: numpy arr converted
+    """
+
+    arr[np.isnan(arr)] = 0
+    arr[arr < -1000] = 0
+    return arr
+
+
+def equalize_hist(arr):
+    """
+    :param arr: must be uint
+    :return:
+    """
+    return cv2.equalizeHist(arr)
+
+
+def morphology_ex(arr):
+    return cv2.morphologyEx(arr)
+
+
+if __name__ == '__main__':
+    f_list = util.get_file_names_in_folder('B:\\preciptation\\train-002', 'npy')
+    arr = np.load(f_list[0])
+    print(np.shape(arr[..., 0]))
+    plt.imshow(arr[..., 0])
+    # a = equalize_hist(arr[..., 0], )
+    # cv2.imshow('hist', a)
+    img = arr[..., 6]
+    e = cv2.equalizeHist(img.astype(np.uint8))
+    plt.subplot(131)
+    plt.imshow(img, cmap='gray')
+    plt.subplot(132)
+    plt.imshow(e, cmap='gray')
+    plt.subplot(133)
+    plt.imshow(arr[...,14], cmap='gray')
--- venv/Scripts/Activate.ps1
+++ venv/Scripts/Activate.ps1
@@ -27,7 +27,7 @@ function global:deactivate ([switch]$NonDestructive) {
 
 deactivate -nondestructive
 
-$env:VIRTUAL_ENV="C:\Users\DSKIM\PycharmProjects\Forecasting\venv"
+$env:VIRTUAL_ENV="C:\Users\dosp0\PycharmProjects\Precipitation-Prediction\venv"
 
 if (! $env:VIRTUAL_ENV_DISABLE_PROMPT) {
     # Set the prompt to include the env name
