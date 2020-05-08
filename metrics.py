9fa3746954dd198b4e81520c423ba75e2c6bb17f
dosp0911 <dosp0911@gmail.com>
2020-05-08 17:20:20 +0900

    metirc

--- metrics.py
+++ metrics.py
@@ -2,3 +2,12 @@ import torch
 
 def MAE(y, y_):
     return torch.abs(y - y_)
+
+def categorical_mse_2Dloss(classes, y_pred, y):
+    """
+    :param classes: (N,H,W) classes index : int
+    :param y_pred: (N,C,H,W) float values
+    :param y: (N,H,W) target float values
+    :return: mean((y_pred[classes] - y) ** 2)
+    """
+    return torch.mean((y_pred[classes] - y)**2)
