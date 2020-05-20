096b42d6d5ff75abb708c288a1236b52274c15dc
dosp0911 <dosp0911@gmail.com>
2020-05-20 20:32:33 +0900

    Deeplab_v3_resnet101 pretrained add

--- gpm_dataset.py
+++ gpm_dataset.py
@@ -6,6 +6,7 @@ import numpy as np
 import cv2
 from pre_processing import handle_outliers, equalize_hist
 
+
 class GpmDataset(Dataset):
     def __init__(self, f_paths, t_f=None):
         super(GpmDataset, self).__init__()
@@ -32,16 +33,39 @@ class GpmDataset(Dataset):
     def __len__(self):
         return len(self.file_list)
 
+class TestGpmDataset(Dataset):
+    def __init__(self, f_paths, t_f=None):
+        super(TestGpmDataset, self).__init__()
+        # self.f_path = f_path
+        self.t_f = t_f
+        self.file_list = f_paths
+
+    def __getitem__(self, item):
+        # test gpm_data = (40, 40, 14)
+        gpm_data = util.load_npy_files(str(self.file_list[item]))
+        assert gpm_data.shape == (40, 40, 14)
+
+        types = torch.from_numpy((gpm_data[..., 9] // 100).astype(int))
+        # remove 9th: types, 12,13th: (DPR LONG/LATITUDE)
+        gpm_data = np.delete(gpm_data, (9,12,13), axis=2)
+        gpm_data = handle_outliers(gpm_data)
+        # opecv histogram equalization , morphologyEx
+        # gpm_data[..., :9] = equalize_hist(gpm_data[..., :9]) # apply equalize histogram on only image channels
+        # pre-processing gpm data ( normalize, brightness, noisy, to_tensor.. so on)
+        gpm_data = self.t_f(gpm_data)
+
+        return gpm_data, types
+
+    def __len__(self):
+        return len(self.file_list)
+
 
 if __name__ == '__main__':
     from sklearn.model_selection import train_test_split
     from torch.utils.data import DataLoader
     # except types, DPR LONG/LATITUDE, Precipitation
-    avg_mean = [1.96452e+02, 1.39343e+02, 2.16165e+02, 1.68963e+02, 2.38547e+02, 2.32316e+02, 1.91321e+02, 2.63215e+02,
-                2.44779e+02,
-                0.0, 0.0]
-    avg_std = [45.087, 72.679, 36.286, 58.972, 29.526, 27.983, 47.61, 23.429, 30.96,
-               180.0, 180.0]
+    avg_mean = [1.96452433e+02, 1.39342924e+02, 2.16165195e+02, 1.68963282e+02, 2.38546964e+02, 2.32315516e+02, 1.91320695e+02, 2.63214598e+02, 2.44779303e+02]
+    avg_std = [45.08675422, 72.6790566, 36.28563412, 58.97204602, 29.52567292, 27.98306607, 47.61029946, 23.42907424, 30.96005726]
 
     t = transforms.Compose(
         [transforms.ToTensor(),
@@ -50,9 +74,10 @@ if __name__ == '__main__':
 
     split_ratio = 0.2
     n_batch = 64
-    DATA_PATH = 'D:\\forecasting\\train-002'
+    # DATA_PATH = 'D:\\forecasting\\train-002'
+    DATA_PATH = 'B:\\preciptation\\train-002'
     f_paths = util.get_file_names_in_folder(DATA_PATH, 'npy')
-    train_paths, val_paths = train_test_split(f_paths, test_size=split_ratio)
+    train_paths, val_paths = train_test_split(f_paths[:10], test_size=split_ratio)
 
     train_dataset = GpmDataset(train_paths, t)
     val_dataset = GpmDataset(val_paths, t)
--- model.py
+++ model.py
@@ -2,127 +2,25 @@
 
 import torch
 import torch.nn as nn
-from torchsummary import summary
-
-import math
 
 
-class Con2D(nn.Module):
-    def __init__(self, in_c, out_c, k_size, is_bn=True):
-        super(Con2D, self).__init__()
-
-        if is_bn:
-            self.sequential = nn.Sequential(
-                nn.Conv2d(in_c, out_c, k_size, padding=1),
-                nn.BatchNorm2d(out_c),
-                nn.ReLU(inplace=True),
-                nn.Conv2d(out_c, out_c, k_size, padding=1),
-                nn.BatchNorm2d(out_c),
-                nn.ReLU(inplace=True)
-            )
+class Deeplab_v3_resnet101_pretraind(nn.Module):
+    def __init__(self, in_channels, out_channels, is_freeze=True, pretrained=True):
+        super(Deeplab_v3_resnet101_pretraind, self).__init__()
+        self.model = torch.hub.load('pytorch/vision:v0.5.0', 'deeplabv3_resnet101', pretrained=pretrained)
+        if pretrained:
+            self.model.requires_grad_(not is_freeze)
         else:
-            self.sequential = nn.Sequential(
-                nn.Conv2d(in_c, out_c, k_size, padding=1),
-                nn.ReLU(inplace=True),
-                nn.Conv2d(out_c, out_c, k_size, padding=1),
-                nn.ReLU(inplace=True)
-            )
+            self.model.requires_grad_(is_freeze)
+        self.model.backbone.conv1 = nn.Conv2d(in_channels, 64, 7, padding=3, bias=False)
+        self.model.backbone.conv1.requires_grad_(is_freeze)
+        self.model.classifier[4] = nn.Conv2d(256, out_channels, 3, padding=1, bias=False)
+        self.model.classifier[4].requires_grad_(is_freeze)
+        self.model.aux_classifier = None
 
     def forward(self, x):
-        return self.sequential(x)
-
-
-class SamePadding2D(nn.Module):
-    def __init__(self):
-        super(SamePadding2D, self).__init__()
-
-    # x : [N,C,H,W]
-    def forward(self, x):
-        h = x.size()[-2]
-        w = x.size()[-1]
-        # in case both of height, width are odd
-        if (h % 2 != 0) and (w % 2 != 0):
-            return nn.ZeroPad2d((0, 1, 0, 1))(x)
-        # in case height is odd, width is even
-        elif (h % 2 != 0) and (w % 2 == 0):
-            return nn.ZeroPad2d((0, 0, 0, 1))(x)
-        # in case height is even, width is odd
-        elif (h % 2 == 0) and (w % 2 != 0):
-            return nn.ZeroPad2d((0, 1, 0, 0))(x)
-        # in case both of height, width are even
-        else:
-            return x
-
-
-def crop(features, size):
-    h_old, w_old = features[0][0].size()
-    h, w = size
-    if (h_old < h) or (w_old < w):
-        raise ValueError('being cropped arr size is smaller than to being added arr')
-    x = math.ceil((h_old - h) / 2)
-    y = math.ceil((w_old - w) / 2)
-    return features[:, :, x:(x + h), y:(y + w)]
-
-
-class U_net(nn.Module):
-    def __init__(self, in_channles, out_channels):
-        super(U_net, self).__init__()
-
-        self.con_block_1 = Con2D(in_channles, 64, 3)
-        self.con_block_2 = Con2D(64, 128, 3)
-        self.con_block_3 = Con2D(128, 256, 3)
-        self.con_block_4 = Con2D(256, 512, 3)
-        self.con_block_5 = Con2D(512, 1024, 3)
-
-        self.exp_block_4 = Con2D(1024, 512, 3)
-        self.exp_block_3 = Con2D(512, 256, 3)
-        self.exp_block_2 = Con2D(256, 128, 3)
-        self.exp_block_1 = Con2D(128, 64, 3)
-
-        self.deconv_4 = nn.ConvTranspose2d(1024, 512, 2, stride=2, output_padding=1)
-        self.deconv_3 = nn.ConvTranspose2d(512, 256, 2, stride=2)
-        self.deconv_2 = nn.ConvTranspose2d(256, 128, 2, stride=2)
-        self.deconv_1 = nn.ConvTranspose2d(128, 64, 2, stride=2)
-
-        self.final_layer = nn.Conv2d(64, out_channels, 1)
-
-    def forward(self, x):
-        con_block_1_out = self.con_block_1(x)
-        x = nn.MaxPool2d(2, stride=2)(con_block_1_out)
-
-        con_block_2_out = self.con_block_2(x)
-        x = nn.MaxPool2d(2, stride=2)(con_block_2_out)
-
-        con_block_3_out = self.con_block_3(x)
-        x = nn.MaxPool2d(2, stride=2)(con_block_3_out)
-
-        con_block_4_out = self.con_block_4(x)
-        x = nn.MaxPool2d(2, stride=2)(con_block_4_out)
-        x = self.con_block_5(x)
-
-        x = self.deconv_4(x)
-        x = torch.cat([crop(con_block_4_out, (x.size()[2], x.size()[3])), x], dim=1)
-        x = self.exp_block_4(x)
-
-        x = self.deconv_3(x)
-        x = torch.cat([crop(con_block_3_out, (x.size()[2], x.size()[3])), x], dim=1)
-        x = self.exp_block_3(x)
-
-        x = self.deconv_2(x)
-        x = torch.cat([crop(con_block_2_out, (x.size()[2], x.size()[3])), x], dim=1)
-        x = self.exp_block_2(x)
-
-        x = self.deconv_1(x)
-        x = torch.cat([crop(con_block_1_out, (x.size()[2], x.size()[3])), x], dim=1)
-        x = self.exp_block_1(x)
-
-        x = self.final_layer(x)
-
-        return x
+        return self.model(x)['out']
 
 
 if __name__ == '__main__':
-    from torchsummary import summary
-    u_net = U_net(12, 1)
-    s = summary(u_net, (12, 40, 40), device='cpu')
-    print(s)
+    m = Deeplab_v3_resnet101_pretraind(9, 12)
